ec236d8b2235ea51ab77b73ca28d90a28dca715c
Enoch2k2 <enoch2k2@hotmail.com>
2018-04-12 09:57:38 -0500

    updated README and added requirements.txt

--- README.md
+++ README.md
@@ -3,12 +3,9 @@
 ### Requirements
 Make sure to have pip installed locally. I suggest having pyenv installed, and python version 2.9.10 as it should have pip installed automatically.
 
-You will need to install these dependencies for this to work.
+You will need to install the dependencies for this to work. (ignore Requirement already satisfied messages)
 ```
-pip install "ipython[notebook]"
-pip install "plotly"
-pip install "pandas"
-pip install "xlrd"
+pip install -r requirements.txt
 ```
 
 ### To run program
--- lib/graph.py
+++ lib/graph.py
@@ -7,7 +7,21 @@ def create_graph():
   file_name = cwd + "/lib/cities.xlsx"
   travel_df = pandas.read_excel(file_name)
   cities = travel_df.to_dict("records")
-  x_values = [cities[0]['City'], cities[1]['City'], cities[2]['City']]
-  y_values = [cities[0]['Population'], cities[1]['Population'], cities[2]['Population']]
-  trace_first_three_pops = {'x': x_values, 'y': y_values}
+  city_names = grab_all_names(cities)
+  city_populations = grab_all_populations(cities)
+  x_values = city_names
+  y_values = city_populations
+  trace_first_three_pops = {'type': 'bar', 'x': x_values, 'y': y_values}
   plotly.offline.plot([trace_first_three_pops])
+
+def grab_all_names(cities):
+  names = []
+  for city in cities:
+    names.append('{0} ({1})'.format(city['City'], city['Country']))
+  return names
+
+def grab_all_populations(cities):
+  population = []
+  for city in cities:
+    population.append(city['Population'])
+  return population
